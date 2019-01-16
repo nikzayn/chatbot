1e8d5513f7998e48e5321c8cff64aef6e38f14ad
Nikhil <nikhilvaidyar1997@gmail.com>
2019-01-16 16:26:58 +0530

    Updates

---
+++ precache-manifest.274344beb0169e1728ff4df0e6e7593c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5da36feb517f3cd8b15f",
+    "url": "/chatbot/static/js/main.5da36feb.chunk.js"
+  },
+  {
+    "revision": "17c11936056651effdcc",
+    "url": "/chatbot/static/js/1.17c11936.chunk.js"
+  },
+  {
+    "revision": "c911c9c2ac5122894384",
+    "url": "/chatbot/static/js/runtime~main.c911c9c2.js"
+  },
+  {
+    "revision": "06969c68359c5cf04ae8003338cd268e",
+    "url": "/chatbot/index.html"
+  }
+];
